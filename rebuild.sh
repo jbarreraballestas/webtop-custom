40be684a77e2477c0b75be3f7f02a67a10a62fb2
Jhordy Barrera <info@jhordybarrera.dev>
2025-05-22 20:53:27 +0000

    Cambiar orden para eliminar directorio despues de detener

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 read -p "¿Eliminar carpeta ./config local? (y/N): " wipe
+docker compose down --volumes --remove-orphans
 if [[ "$wipe" == "y" ]]; then
   rm -rf ./config
 fi
-
-docker compose down --volumes --remove-orphans
 docker compose build --no-cache
 docker compose up -d
